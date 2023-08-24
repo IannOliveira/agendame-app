51498e8db01d6cb107ddaee7debc5ecc1887349c
Iann Oliveira <ianncosta13@gmail.com>
2023-08-24 14:17:18 -0300

    Criação do Login e Password junto com as credenciais para autenticação

---
+++ src/main.js
@@ -0,0 +1,31 @@
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+import './axios'
+
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+// Plugins
+import vuetify from "@/plugins/vuetify";
+import router from "@/router";
+import pinia from "@/store";
+import {useMe} from "@/store/me";
+
+const app = createApp(App)
+
+app.use(pinia)
+const meStore = useMe();
+meStore.getMe()
+  .catch(() =>{})
+  .finally( () => {
+app.use(vuetify)
+app.use(router)
+app.mount('#app')
+  })
---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <VContainer>
+    <VTextField v-model="email"/>
+    <VTextField v-model="password"/>
+    <v-btn @click="login">Login</v-btn>
+  </VContainer>
+</template>
+
+<script setup>
+
+import {useAuth} from "@/store/auth";
+import {ref} from "vue";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+const authStore = useAuth();
+
+const email = ref('test@example.com');
+const password = ref('password');
+
+function login(){
+  authStore.login(email.value, password.value).then(() => {
+    router.push({name: 'dashboard'});
+  });
+}
+
+</script>
