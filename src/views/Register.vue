9cc837db3977bb2f6696ca3684d4605e4cdd4789
Iann Oliveira <ianncosta13@gmail.com>
2023-08-24 16:30:41 -0300

    Adicionado rota de registro

---
+++ src/views/Register.vue
@@ -0,0 +1,27 @@
+<template>
+  <VContainer>
+    <VTextField v-model="firstName"/>
+    <VTextField v-model="lastName"/>
+    <VTextField type="email" v-model="email"/>
+    <VTextField type="password" v-model="password"/>
+    <v-btn @click="register">Cadastrar</v-btn>
+  </VContainer>
+</template>
+
+  <script setup>
+
+    import {ref} from "vue";
+    import { useAuth } from "@/store/auth";
+
+    const firstName = ref('');
+    const lastName = ref('');
+    const email = ref('');
+    const password = ref('');
+
+    function register() {
+      const authStore = useAuth();
+      authStore.register(firstName.value, lastName.value, email.value, password.value)
+    }
+
+  </script>
+
