390f5eafd30ff9431ad9d6f9e751d2812934878c
Iann Oliveira <ianncosta13@gmail.com>
2023-09-01 15:58:29 -0300

    Inicializado a ingracao 'subscription' do stribe

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,27 @@
 <template>
   <VContainer>
-    Dashboard
+    Dashboard <br>
+
+    <VBtn @click="subscribe()" :loading="loading">Assinar</VBtn>
   </VContainer>
 </template>
 
 <script setup>
+import {useSubscription} from "@/store/subscription";
+import {ref} from "vue";
+
+const loading = ref(false);
+function subscribe(){
+  loading.value = true;
+  const subscriptionStore = useSubscription()
+  subscriptionStore.subscribe().then((response) => {
+    if(response){
+     window.location.href = response;
+    }
+   }).catch(() => {
+     loading.value= false;
+  })
+
+}
 
 </script>
