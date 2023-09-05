72685e389d8dbdc4563f18c928026b2e14729c6e
Iann Oliveira <ianncosta13@gmail.com>
2023-09-05 15:46:54 -0300

    adicionado roles e permissions

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,16 @@
 <template>
   <VContainer>
     Dashboard <br>
+    <VBtn v-if="can('edit_post')">Editar post</VBtn>
+    <br>
+    <VBtn v-if="can('delete_user')">Deletar user</VBtn>
+    <pre>{{user}}</pre>
+
   </VContainer>
 </template>
 
 <script setup>
+import { useMe} from "@/store/me";
 
-
+const { user, can } = useMe();
 </script>
